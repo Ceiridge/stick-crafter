5b1d6725a6409dc2f3000ccc74a39db453b09e5e
Ceiridge <philipp34533@icloud.com>
2020-08-28 23:51:15 +0200

    Update: Set fabric properties and created first classes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Stick Crafter
 
-A fabric mod for assisting in crafting sticks from logs.
+A fabric mod for assisting in crafting sticks from logs. Currently only supports Minecraft 1.16.1 + Fabric + Fabric API
---
+++ src/main/java/org/ceiridge/stickcrafter/StickCrafter.java
@@ -0,0 +1,11 @@
+package org.ceiridge.stickcrafter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StickCrafter implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
